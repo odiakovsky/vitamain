479fe3bccdbbbf1b055704714c59397f8f925bd8
Oleg Dyakovskii <odiakovsky@gmail.com>
2023-04-06 22:01:04 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
     var navLinks = document.querySelectorAll('.nav h3');
-    navLinks.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        var targetId = this.getAttribute('href');
-        var targetElem = document.querySelector(targetId);
-        var offsetTop = targetElem.offsetTop;
-        window.scrollTo({
-          top: offsetTop,
-          behavior: 'smooth'
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetId = this.getAttribute('href');
+            var targetElem = document.querySelector(targetId);
+            var offsetTop = targetElem.offsetTop;
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
         });
-      });
     });
-  });
-  
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var navLinks = document.querySelectorAll('.nav-mobile h3');
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetId = this.getAttribute('href');
+            var targetElem = document.querySelector(targetId);
+            var offsetTop = targetElem.offsetTop;
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+
+
+const burger = document.querySelector(".burger");
+const navMobile = document.querySelector(".nav-mobile");
+const krest = document.querySelector(".krest");
+
+burger.addEventListener("click", () => {
+    navMobile.classList.toggle("active");
+});
+
+krest.addEventListener("click", () => {
+    navMobile.classList.remove("active");
+    burger.style.display = "flex";
+});
+
+
